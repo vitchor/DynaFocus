79f928c103d3de9f09ff42a4e4f7dc89637d2b6b
Victor <vitchor@gmail.com>
2012-07-10 19:01:56 -0300

    Added xcode project

---
+++ DynaFocus/DynaFocus/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  DynaFocus
+//
+//  Created by Marcia  Rozenfeld on 7/10/12.
+//  Copyright (c) 2012 Ufscar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ DynaFocus/DynaFocus/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  DynaFocus
+//
+//  Created by Marcia  Rozenfeld on 7/10/12.
+//  Copyright (c) 2012 Ufscar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
