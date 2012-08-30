850cb94c1ffa5acfeec13ab622cc95cdb2b16e4d
Alexandre Cordeiro <alexandre@pompeii.local>
2012-08-30 16:02:16 -0300

    Added _coming soon_ to Friends and Profile tabs

---
+++ DyFocus/DyFocus/FriendsController.m
@@ -0,0 +1,38 @@
+//
+//  FriendsController.m
+//  DyFocus
+//
+//  Created by Alexandre Cordeiro on 8/30/12.
+//  Copyright (c) 2012 Ufscar. All rights reserved.
+//
+
+#import "FriendsController.h"
+
+@interface FriendsController ()
+
+@end
+
+@implementation FriendsController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
