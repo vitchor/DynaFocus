fb009f90ac335281be5c7937b34e065cb43baa74
marcelosalloum <marcelosalloum@gmail.com>
2013-01-24 01:59:34 -0200

    At the (Facebook) login view, added two arrows allowing the user to see different FOF examples. Also, I added 3 more FOFs that the user can check at this view.

--- DyFocus/DyFocus/LoginController.m
+++ DyFocus/DyFocus/LoginController.m
@@ -16,7 +16,7 @@
 
 @implementation LoginController
 
-@synthesize firstImageView,secondImageView, frames, timer, borderView, facebookConnectButton;
+@synthesize firstImageView,secondImageView, frames, timer, borderView, facebookConnectButton, leftButton, rightButton, fofs;
 
 #define TIMER_INTERVAL 0.1;
 #define TIMER_PAUSE 10.0 / TIMER_INTERVAL;
@@ -27,19 +27,7 @@
     if (self) {
         // Custom initialization
         
-        frames = [[NSMutableArray alloc] initWithCapacity:3];
-        
-        UIImage *frame = [UIImage imageNamed:@"example_fof_0.jpeg"];
-        [frames addObject:frame];
-        
-        frame = [UIImage imageNamed:@"example_fof_1.jpeg"];
-        [frames addObject:frame];
-        
-        frame = [UIImage imageNamed:@"example_fof_2.jpeg"];
-        [frames addObject:frame];
-        
-       
-        
+        [self initializeFofs];
         //borderView.layer setCornerRadius
         //borderView.layer.masksToBounds = YES;
         
@@ -47,8 +35,50 @@
     return self;
 }
 
+- (void)initializeFofs{
+    fofs = [[NSMutableArray alloc] initWithCapacity:3];
+
+    NSMutableArray *m_frames0 = [[NSMutableArray alloc] initWithCapacity:3];
+    UIImage *frame = [UIImage imageNamed:@"fof_example_00_1.jpeg"];
+    [m_frames0 addObject:frame];
+    frame = [UIImage imageNamed:@"fof_example_00_2.jpeg"];
+    [m_frames0 addObject:frame];
+    frame = [UIImage imageNamed:@"fof_example_00_3.jpeg"];
+    [m_frames0 addObject:frame];
+    [fofs addObject:m_frames0];
+    
+    NSMutableArray *m_frames1 = [[NSMutableArray alloc] initWithCapacity:2];
+    frame = [UIImage imageNamed:@"fof_example_01_1.jpeg"];
+    [m_frames1 addObject:frame];
+    frame = [UIImage imageNamed:@"fof_example_01_2.jpeg"];
+    [m_frames1 addObject:frame];
+    [fofs addObject:m_frames1];
+
+    NSMutableArray *m_frames2 = [[NSMutableArray alloc] initWithCapacity:2];
+    frame = [UIImage imageNamed:@"fof_example_02_1.jpeg"];
+    [m_frames2 addObject:frame];
+    frame = [UIImage imageNamed:@"fof_example_02_2.jpeg"];
+    [m_frames2 addObject:frame];
+    [fofs addObject:m_frames2];
+    
+    NSMutableArray *m_frames3 = [[NSMutableArray alloc] initWithCapacity:2];
+    frame = [UIImage imageNamed:@"fof_example_03_1.jpeg"];
+    [m_frames3 addObject:frame];
+    frame = [UIImage imageNamed:@"fof_example_03_2.jpeg"];
+    [m_frames3 addObject:frame];
+    [fofs addObject:m_frames3];
+
+    fofIndex = 1;
+    [self refreshFrames];
+}
+
+- (void) refreshFrames{
+    frames = [fofs objectAtIndex:fofIndex];
+}
+
 - (void)viewDidLoad
 {
+    NSMutableArray *frames = [fofs objectAtIndex:0];
     [super viewDidLoad];
 
     [self.firstImageView setImage: [self.frames objectAtIndex:0]];
@@ -64,6 +94,8 @@
     timerPause = TIMER_INTERVAL;
     
     [facebookConnectButton addTarget:self action:@selector(connectWithFacebook) forControlEvents:UIControlEventTouchUpInside];
+    [leftButton addTarget:self action:@selector(showsPreviousFof) forControlEvents:UIControlEventTouchUpInside];
+    [rightButton addTarget:self action:@selector(showsNextFof) forControlEvents:UIControlEventTouchUpInside];
     
 
     timer = [NSTimer scheduledTimerWithTimeInterval:0.01 target:self selector:@selector(fadeImages) userInfo:nil repeats:YES];
@@ -76,6 +108,34 @@
     AppDelegate* appDelegate = [UIApplication sharedApplication].delegate;
     [appDelegate signin];
     
+}
+
+- (void) showsPreviousFof {
+    int fofSize = [fofs count];
+    fofIndex = fofIndex - 1;
+    if (fofIndex < 0) {
+        fofIndex = fofSize - 1;
+    }
+    [self refreshFrames];
+    
+    oldFrameIndex = 0;
+    
+    [self.secondImageView setImage:[self.frames objectAtIndex:0]];
+    [self.firstImageView setImage:[self.frames objectAtIndex:1]];
+}
+
+- (void) showsNextFof {
+    int fofSize = [fofs count];
+    fofIndex = fofIndex + 1;
+    if (fofIndex >= fofSize) {
+        fofIndex = 0;
+    }
+    [self refreshFrames];
+    
+    oldFrameIndex = 0;
+    
+    [self.secondImageView setImage:[self.frames objectAtIndex:0]];
+    [self.firstImageView setImage:[self.frames objectAtIndex:1]];
 }
 
 - (void)fadeImages
