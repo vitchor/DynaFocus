f85f74b34b54363de9db9a053123b441764e570a
Victor Oliveira <vitchor@gmail.com>
2013-03-08 19:01:03 -0300

    Add new notification classes

---
+++ DyFocus/DyFocus/NotificationTableViewCell.m
@@ -0,0 +1,29 @@
+//
+//  NotificationTableViewCell.m
+//  DyFocus
+//
+//  Created by Victor on 3/8/13.
+//  Copyright (c) 2013 dyfocus. All rights reserved.
+//
+
+#import "NotificationTableViewCell.h"
+
+@implementation NotificationTableViewCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
